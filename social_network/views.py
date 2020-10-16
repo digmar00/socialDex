85235eb3f82dbb18c2293b794ad88eb4b344af86
Marco <marcodignazio.md2@gmail.com>
2020-10-16 22:49:35 +0200

    I fixed a bug related to user tracking

--- social_network/views.py
+++ social_network/views.py
@@ -11,7 +11,7 @@ def home(request):
     if request.user.is_authenticated:
         ip = get_client_ip(request)
 
-        if ip != request.user.profile.last_ip_used:
+        if (ip != request.user.profile.last_ip_used) & (request.user.profile.last_ip_used is not None):
             Profile.objects.filter(user=request.user).update(is_ip_changed=True)
 
         Profile.objects.filter(user=request.user).update(last_ip_used=ip)
